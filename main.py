83be68d848e6ecbbc858579645fab7416ac712d2
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-01-09 17:13:27 +0000

    added certain command on request

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 import os
 from keep_alive import keep_alive
+import random, requests
 
 client = discord.Client()
 
 client = commands.Bot(command_prefix="-")
-
 
 @client.event
 async def on_ready():
@@ -37,8 +37,16 @@ async def on_message(message):
 	
 	if 'nsfw' in message.content:
 		print('Keyword found in message')
-		msg = 'https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'.format(message)
+		msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'.format(message)
 		await client.send_message(message.channel, msg)
+
+	if '-hentai' in message.content:
+		print('Yum time')
+		response = requests.get("https://www.reddit.com/r/hentai.json", headers={"User-Agent": "linux:memebot:v1.0.0"})
+		page = response.json()
+		all_urls = random.choice(page["data"]["children"])["data"]["url"]
+		# msg = 'all_urls'.format(message)
+		await client.send_message(message.channel, all_urls)
 
 keep_alive()
 token = os.environ.get("DISCORD_BOT_SECRET")
