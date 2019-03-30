6546bb1316ce148329e8d6d090e0cb75cfe098e8
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-03-30 21:39:59 +0000

    rewrote to allow better multithreading

--- main.py
+++ main.py
@@ -1,99 +1,72 @@
-import discord
 from discord.ext import commands
 import os
-from keep_alive import keep_alive
 import random, requests
 import json
 import urllib.request
 
-client = discord.Client()
+client = commands.Bot(command_prefix='-')
 
-client = commands.Bot("-")
+token = os.getenv("TOKEN")
 
 @client.event
 async def on_ready():
-	print("I'm in")
-	print(client.user)
+	print('client ready')
 
-class Main_Commands():
-	global bot
-	def __init__(self, bot):
-		self.bot = bot
+@client.command()
+async def brown():
+	msg = 'https://cdn.discordapp.com/attachments/263062760424865792/476401246945935380/oops.jpg'
+	await client.say(msg)
 
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
+@client.command()
+async def cat():
+	print('cats')
+	url = "https://api.thecatapi.com/v1/images/search"
+	response = urllib.request.urlopen(url)
+	result = json.loads(response.read())
 
-	if message.content.startswith('-cat'):
-		print('cats')
-		url = "https://api.thecatapi.com/v1/images/search"
-		response = urllib.request.urlopen(url)
-		result = json.loads(response.read())
+	url = result[0]['url']
+	await client.say(url)
 
-		url = result[0]['url']
-		await client.send_message(message.channel, url)
+@client.command()
+async def support():
+	print('help menu')
+	msg = 'Help menu:\n`-help` - Get help\n`-dab` - Get a dab\n`-hentai` - For the good stuff\n`-meme` - Get a dank meme\n`-cat` - Get a picture of a cat\n`-nsfw` - Gwizz\n`-duck` - Duckle\n'
+	await client.say(msg)
 
-	if message.content.startswith('-help'):
-		print('help menu')
-		msg = 'Help menu:\n`-help` - Get help\n`-dab` - Get a dab\n`-hentai` - For the good stuff\n`-meme` - Get a dank meme\n`-cat` - Get a picture of a cat\n'.format(message)
-		await client.send_message(message.channel, msg)
+@client.command()
+async def dab():
+	print('Dab!')
+	msg = '( ͡° ͜ʖ ͡°)'
+	await client.say(msg)
 
-	if message.content.startswith('-hello'):
-		print('Hello')
-		msg = 'Hello {0.author.mention}'.format(message)
-		await client.send_message(message.channel, msg)
+@client.command()
+async def nsfw():
+	print('Gwizz')
+	msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'
+	await client.say(msg)
 
-	if message.content.startswith('-dab'):
-		print('Dab!')
-		msg = '( ͡° ͜ʖ ͡°)'.format(message)
-		await client.send_message(message.channel, msg)
-		
-	if message.content.startswith('Keep Yourself Safe'):
-		print('Bungus response')
-		msg = 'Thanks Bungus for keeping us safe'.format(message)
-		await client.send_message(message.channel, msg)
+@client.command()
+async def hentai():
+	print('Yum time')
+	response = requests.get("https://www.reddit.com/r/hentai.json", headers={"User-Agent": "linux:memebot:v1.0.0"})
+	page = response.json()
+	all_urls = random.choice(page["data"]["children"])["data"]["url"]
+	msg = all_urls
+	await client.say(msg)
 
-	if message.content.startswith('Ollie is so dumb'):
-		msg = 'Yes, he is.'.format(message)
-		await client.send_message(message.channel, msg)
-	
-	if 'nsfw' in message.content:
-		print('Gwizz')
-		msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'.format(message)
-		await client.send_message(message.channel, msg)
+@client.command()
+async def duck():
+	print('Duck!')
+	msg = 'https://giphy.com/gifs/duck-excited-school-krewXUB6LBja'
+	await client.say(msg)
 
-	if '-hentai' in message.content:
-		print('Yum time')
-		response = requests.get("https://www.reddit.com/r/hentai.json", headers={"User-Agent": "linux:memebot:v1.0.0"})
-		page = response.json()
-		all_urls = random.choice(page["data"]["children"])["data"]["url"]
-		# msg = 'all_urls'.format(message)
-		await client.send_message(message.channel, all_urls)
+@client.command()
+async def meme():
+	print('Meme time')
+	response = requests.get("https://www.reddit.com/r/meme.json", headers={"User-Agent": "linux:memebot:v1.0.0"})
+	page = response.json()
+	all_urls = random.choice(page["data"]["children"])["data"]["url"]
+	msg = all_urls
+	await client.say(msg)
 
-	if 'join' in message.content:
-		print('Join')
-		msg = 'No flip off'.format(message)
-		await client.send_message(message.channel, msg)
-
-	if 'duck' in message.content:
-		print('Duck!')
-		msg = 'https://giphy.com/gifs/duck-excited-school-krewXUB6LBja'.format(message)
-		await client.send_message(message.channel, msg)
-
-	if 'brown' in message.content:
-		print('Brown cat')
-		msg = 'https://cdn.discordapp.com/attachments/263062760424865792/476401246945935380/oops.jpg'.format(message)
-		await client.send_message(message.channel, msg)
-
-	if '-meme' in message.content:
-		print('Meme time')
-		response = requests.get("https://www.reddit.com/r/meme.json", headers={"User-Agent": "linux:memebot:v1.0.0"})
-		page = response.json()
-		all_urls = random.choice(page["data"]["children"])["data"]["url"]
-		# msg = 'all_urls'.format(message)
-		await client.send_message(message.channel, all_urls)
-
-keep_alive()
-token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
