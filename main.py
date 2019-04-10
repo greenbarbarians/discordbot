42eadd11cddbec3833422f8bc0351c3f0f6e3fd5
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-04-10 11:26:56 +0100

    if message contains commands

--- main.py
+++ main.py
@@ -21,19 +21,28 @@ token = os.getenv("TOKEN")
 async def on_ready():
 	print('client ready')
 
-
-@client.command()
-async def brown():
-	print('brown cat')
-	msg = 'https://cdn.discordapp.com/attachments/263062760424865792/476401246945935380/oops.jpg'
-	await client.say(msg)
-
-
-@client.command()
-async def black():
-	print('black cat')
-	msg = 'https://cdn.discordapp.com/attachments/293807439713927169/565473888524304404/image0.jpg'
-	await client.say(msg)
+# if message contains commands
+@client.event
+async def on_message(message):
+	# don't reply to itself
+	if message.author == client.user:
+		return
+	# gwizz
+	if 'nsfw' in message.content:
+		print('Gwizz')
+		msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'
+		await client.send_message(message.channel, msg)
+	# brown cat
+	elif 'brown' in message.content:
+		print('brown cat')
+		msg = 'https://cdn.discordapp.com/attachments/263062760424865792/476401246945935380/oops.jpg'
+		await client.send_message(message.channel, msg)
+	# black cat
+	elif 'black' in message.content:
+		print('black cat')
+		msg = 'https://cdn.discordapp.com/attachments/293807439713927169/565473888524304404/image0.jpg'
+		await client.send_message(message.channel, msg)
+	await client.process_commands(message)
 
 
 @client.command()
@@ -51,13 +60,6 @@ async def cat():
 async def support():
 	print('help menu')
 	msg = 'Help menu:\n`-help` - Get help\n`-dab` - Get a dab\n`-hentai` - For the good stuff\n`-meme` - Get a dank meme\n`-cat` - Get a picture of a cat\n`-nsfw` - Gwizz\n`-duck` - Duckle\n'
-	await client.say(msg)
-
-
-@client.command()
-async def nsfw():
-	print('Gwizz')
-	msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'
 	await client.say(msg)
 
 
@@ -86,5 +88,6 @@ async def meme():
 	all_urls = random.choice(page["data"]["children"])["data"]["url"]
 	msg = all_urls
 	await client.say(msg)
+
 
 client.run(token)
