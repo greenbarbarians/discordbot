130c07a307707448c162807baae2c6da436e2034
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-04-10 10:57:28 +0100

    support for my local machine
    added command for a cat
    
    
    fix error
    
    
    print for cat pictures

--- main.py
+++ main.py
@@ -2,22 +2,39 @@
 
 from discord.ext import commands
 import os
-import random, requests
+import random
+import requests
 import json
 import urllib.request
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 client = commands.Bot(command_prefix='-')
 
 token = os.getenv("TOKEN")
 
+
 @client.event
 async def on_ready():
 	print('client ready')
 
+
 @client.command()
 async def brown():
+	print('brown cat')
 	msg = 'https://cdn.discordapp.com/attachments/263062760424865792/476401246945935380/oops.jpg'
 	await client.say(msg)
+
+
+@client.command()
+async def black():
+	print('black cat')
+	msg = 'https://cdn.discordapp.com/attachments/293807439713927169/565473888524304404/image0.jpg'
+	await client.say(msg)
+
 
 @client.command()
 async def cat():
@@ -29,23 +46,20 @@ async def cat():
 	url = result[0]['url']
 	await client.say(url)
 
+
 @client.command()
 async def support():
 	print('help menu')
 	msg = 'Help menu:\n`-help` - Get help\n`-dab` - Get a dab\n`-hentai` - For the good stuff\n`-meme` - Get a dank meme\n`-cat` - Get a picture of a cat\n`-nsfw` - Gwizz\n`-duck` - Duckle\n'
 	await client.say(msg)
 
-@client.command()
-async def dab():
-	print('Dab!')
-	msg = '( ͡° ͜ʖ ͡°)'
-	await client.say(msg)
 
 @client.command()
 async def nsfw():
 	print('Gwizz')
 	msg = 'Someone say nsfw? I got you https://cdn.discordapp.com/attachments/263062760424865792/532602893287686165/brown_cat.png'
 	await client.say(msg)
+
 
 @client.command()
 async def hentai():
@@ -56,11 +70,13 @@ async def hentai():
 	msg = all_urls
 	await client.say(msg)
 
+
 @client.command()
 async def duck():
 	print('Duck!')
 	msg = 'https://giphy.com/gifs/duck-excited-school-krewXUB6LBja'
 	await client.say(msg)
+
 
 @client.command()
 async def meme():
